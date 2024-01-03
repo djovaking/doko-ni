57b71ef0c68924816e391c675fe906717efed8cc
Rashmi Bhavsar <94361221+rash2615@users.noreply.github.com>
2024-01-03 11:19:28 +0100

    Ajout d'image et de Countdown

--- public/pages/HomePage.js
+++ public/pages/HomePage.js
@@ -1,6 +1,7 @@
 import { Component, createElement } from "../core/MiniReact.js";
 import Header from "../components/Header.js";
 import Section from "../components/Section.js";
+import Countdown from "../components/Countdown.js";
 
 class HomePage extends Component {
   render() {
@@ -8,18 +9,16 @@ class HomePage extends Component {
       createElement(Header, { title: 'Home', to: '/home' }),
       createElement('main', {},
         createElement(Section, {},
-          createElement('div', {class: "home-title"}, 
-          createElement('h1', {}, "Paris 2024"),
-          createElement('h3', {}, "Venez découvrir les lieux des JO de Paris 2024 !"),
-         
-          ),
-        ),
-        createElement(Section, {},
-          createElement('div', {class: "home-title"}, 
-          createElement('h1', {}, "Paris 2024"),
-          createElement('h3', {}, "Venez découvrir les lieux des JO de Paris 2024 !"),
-         
-          ),
+          createElement('div', { class: "home-title" },
+            createElement('img', { 
+              src: './src/media/accueil.png', // Remplacez ceci par le chemin de votre image
+              alt: 'Paris 2024 Image',
+              style: { width: '100%', marginBottom: '20px' } // Style pour la pleine largeur et un espacement en bas
+            }),
+            createElement('h1', {}, "Paris 2024"),
+            createElement('h3', {}, "Venez découvrir les lieux des JO de Paris 2024 !"),
+            createElement(Countdown)
+          )
         ),
       )
     );
