daeebc05676a8fa0251a8f04bb04702ed2d867bd
Rashmi Bhavsar <94361221+rash2615@users.noreply.github.com>
2024-01-03 16:32:16 +0100

    Ajout d'une page About

--- public/pages/about.js
+++ public/pages/about.js
@@ -1,16 +1,32 @@
 import { Component, createElement } from "../core/MiniReact.js";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
+import Section from "../components/Section.js";
 
 class About extends Component {
-
   render() {
     return createElement(
       "div",
-      {},
-      createElement(Header),
-      createElement("h1", {}, "About"),
-      createElement(Footer)
+      { class: "about-page" },
+      createElement(Header, {title:'A Propos', to: '/about' }),
+      createElement(
+        "div",
+        { className: "about-content" },
+        createElement("h1", {}, "À propos des Jeux olympiques 2024 à Paris"),
+        createElement("p", {}, "Bienvenue sur notre site dédié aux Jeux olympiques de 2024 à Paris!"),
+        createElement("p", {}, "Cet événement mondial est prévu pour célébrer l'excellence sportive et la diversité culturelle."),
+        createElement("p", {}, "Notre objectif est de fournir des informations détaillées sur les lieux, les événements et les athlètes pour vous permettre de profiter pleinement de cette grande célébration sportive."),
+        createElement("p", {}, "Explorez notre site pour en savoir plus sur les Jeux olympiques de 2024 à Paris!"),
+        createElement(
+          "div",
+          { class: "button-container" },
+          createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/events'; } }, "Voir les événements"),
+          createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/spots'; } }, "Voir les spots")
+
+        ),
+        createElement(Footer)
+      )
+      
     );
   }
 }
