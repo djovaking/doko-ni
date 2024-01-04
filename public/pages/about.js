da9d8b2362e6f3a072cc3ee01c1329bc8a31b674
Rashmi Bhavsar <94361221+rash2615@users.noreply.github.com>
2024-01-04 16:54:35 +0100

    Correction de la mise en page de la page "About"

--- public/pages/about.js
+++ public/pages/about.js
@@ -5,28 +5,29 @@ import Section from "../components/Section.js";
 
 class About extends Component {
   render() {
-    return createElement(
-      "div",
-      { class: "about-page" },
-      createElement(Header, {title:'A Propos', to: '/about' }),
-      createElement(
-        "div",
-        { className: "about-content" },
-        createElement("h1", {}, "À propos des Jeux olympiques 2024 à Paris"),
-        createElement("p", {}, "Bienvenue sur notre site dédié aux Jeux olympiques de 2024 à Paris!"),
-        createElement("p", {}, "Cet événement mondial est prévu pour célébrer l'excellence sportive et la diversité culturelle."),
-        createElement("p", {}, "Notre objectif est de fournir des informations détaillées sur les lieux, les événements et les athlètes pour vous permettre de profiter pleinement de cette grande célébration sportive."),
-        createElement("p", {}, "Explorez notre site pour en savoir plus sur les Jeux olympiques de 2024 à Paris!"),
-        createElement(
-          "div",
-          { class: "button-container" },
-          createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/events'; } }, "Voir les événements"),
-          createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/spots'; } }, "Voir les spots")
-
+    return createElement('div', {},
+      createElement(Header, { title: 'A Propos', to: '/about' }),
+      createElement('main', {},
+        createElement(Section, {},
+          createElement(
+            "div",
+            { class: "about-content" },
+            createElement("h1", {}, "À propos des Jeux olympiques 2024 à Paris"),
+            createElement("p", {}, "Bienvenue sur notre site dédié aux Jeux olympiques de 2024 à Paris!"),
+            createElement("p", {}, "Cet événement mondial est prévu pour célébrer l'excellence sportive et la diversité culturelle."),
+            createElement("p", {}, "Notre objectif est de fournir des informations détaillées sur les lieux, les événements et les athlètes pour vous permettre de profiter pleinement de cette grande célébration sportive."),
+            createElement("p", {}, "Explorez notre site pour en savoir plus sur les Jeux olympiques de 2024 à Paris!"),
+            createElement(
+              "div",
+              { class: "button-container" },
+              createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/events'; } }, "Voir les événements"),
+              createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/spots'; } }, "Voir les spots")
+    
+            ),
         ),
         createElement(Footer)
       )
-      
+    )
     );
   }
 }
