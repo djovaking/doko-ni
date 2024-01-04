5300620cc84ef178cbf9348188c3047b17490967
Rashmi Bhavsar <94361221+rash2615@users.noreply.github.com>
2024-01-04 22:29:10 +0100

    Ajout des images dans About

--- public/pages/about.js
+++ public/pages/about.js
@@ -20,14 +20,22 @@ class About extends Component {
             createElement(
               "div",
               { class: "button-container" },
-              createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/events'; } }, "Voir les événements"),
-              createElement("button", { class: "redirect-button", onclick: () => { window.location.href = '/spots'; } }, "Voir les spots")
-    
+              createElement("button", { class: "redirect-button", onclick: () => { window.location.href = `${window.location.pathname}/events`; } }, "Voir les événements"),
+              createElement("button", { class: "redirect-button", onclick: () => { window.location.href = `${window.location.pathname}/spots`; } }, "Voir les spots")
+
             ),
-        ),
+            createElement("div",
+            {class: "image-container"},
+            
+            createElement("img", {src: "https://media.istockphoto.com/id/1568979488/fr/photo/anneaux-olympiques-devant-lh%C3%B4tel-de-ville-au-printemps-2023-paris-france.jpg?s=612x612&w=0&k=20&c=XQ4MTCfyZteY3852gNiLKz5ObkBnaXDgc-89ra76IQI=", alt: "Logo JO 2024",height:"250"}),
+            createElement("img", {src: "https://medias.paris2024.org/uploads/2022/04/GettyImages-82508119-scaled.jpg?x-oss-process=image/resize,w_2560,h_1848,m_lfit/format,jpeg", alt: "Logo JO 2024" ,height:"250"}),
+            createElement("img", {src: "https://media.istockphoto.com/id/1298559355/fr/photo/drapeaux-du-monde-ondulant-dans-le-vent.jpg?s=612x612&w=0&k=20&c=adTv_q4CR-BldzqS7Pt8QNcu9Yn4ZGlpAycxLzlXfhk=", alt: "Logo JO 2024" ,height:"250"})
+            
+           ),
         createElement(Footer)
       )
     )
+      )
     );
   }
 }
